1ab83b3ba4c22e37f9ce19708de1243ef2532364
Er. Mohammad <adilsaascraft@gmail.com>
2026-01-23 15:40:38 +0530

    Change grid layout from 3 to 4 columns

--- app/(home)/upcoming/page.tsx
+++ app/(home)/upcoming/page.tsx
@@ -185,7 +185,7 @@ const {
       </div>
 
       {/* CARDS */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         {paginatedWebinars.map((w) => {
           const isRegistered = registeredIds.includes(w._id)
 
